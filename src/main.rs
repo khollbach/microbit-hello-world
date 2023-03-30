3fe3f558c2723cae16ebe851339d5d9975399d38
Kevan Hollbach <khollbach@gmail.com>
2023-03-30 14:49:01 -0700

    Get it to build and flash
    The magic incantation is: `cargo embed --target thumbv7em-none-eabihf`
    
    Note that one of the configuration files is hidden, in `.cargo/config`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use microbit as _;
+use panic_halt as _;
+
+#[entry]
+fn main() -> ! {
+    panic!();
 }
