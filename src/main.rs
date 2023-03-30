920232ed1ef3f1a1b717b92d62954d2fabe66498
Kevan Hollbach <khollbach@gmail.com>
2023-03-30 15:07:53 -0700

    Add blinky code

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,35 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-use microbit as _;
+use microbit::{display::blocking::Display, hal::Timer, Board};
 use panic_halt as _;
 
 #[entry]
 fn main() -> ! {
-    panic!();
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+
+    loop {
+        display.show(&mut timer, ALL_ON, DELAY_MS);
+        display.show(&mut timer, ALL_OFF, DELAY_MS);
+    }
 }
+
+const DELAY_MS: u32 = 1_000;
+
+const ALL_ON: [[u8; 5]; 5] = [
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+];
+
+const ALL_OFF: [[u8; 5]; 5] = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+];
